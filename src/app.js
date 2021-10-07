8b2e138460efb024616770dcb693d782c4968ae0
bastianmoral <bastihc@gmail.com>
2021-10-07 13:56:44 -0300

    RANDOM EXCUSE GENERATOR BASTIAN MORAL

--- src/app.js
+++ src/app.js
@@ -1,11 +1,67 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //document.querySelector("#refresh").addEventListener("click", () => {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+} /*)*/;
+console.log("Hello from the console of Bastián");
+/*};*/
+
+let generateExcuse = () => {
+  let who = [
+    "The dog",
+    "My grandma",
+    "His turtle",
+    "My bird",
+    "my crazy uncle",
+    "the cat lady"
+  ];
+  let action = [
+    "ate",
+    "peed",
+    "crushed",
+    "broke",
+    "threw",
+    "took",
+    "scream at",
+    "stole",
+    "bit"
+  ];
+  let what = [
+    "my homework",
+    "the keys",
+    "the car",
+    "my shoes",
+    "my laptop",
+    "my switch",
+    "my ps4"
+  ];
+  let when = [
+    "before the class",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+    "on the street",
+    "in my balcony",
+    "in the shower"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
